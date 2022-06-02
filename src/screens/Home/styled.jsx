7b4e883b660b305b993224d0d471069b99370984
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-06-01 23:36:24 -0300

    fix to text home

--- src/screens/Home/styled.jsx
+++ src/screens/Home/styled.jsx
@@ -4,7 +4,6 @@ export const ContainerHome = styled.div`
   width: 100%;
   background: grey;
   height: 100vh;
-  font-size: 5rem;
   display: flex;
   justify-content: center;
   align-items: center;
