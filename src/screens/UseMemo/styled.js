a14e19ea6456215ab8e44e225df3b9eee042527b
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-06-19 03:06:06 -0300

    conflict resolution

--- src/screens/UseMemo/styled.js
+++ src/screens/UseMemo/styled.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import Container from "../../components/Container";
 
 export const ContainerUseMemo = styled(Container)`
-  backdrop-filter: blur(15px);
   background: rgba(39, 1, 64, 0.57);
+  justify-content: space-between;
+  backdrop-filter: blur(15px);
   border-radius: 10px;
   width: 90%;
   height: 100vh;
   padding: 30px;
   display: flex;
-  justify-content: space-between;
 `;
